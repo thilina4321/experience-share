c046c0a30c0593be3a6401a31bd937ae5eed09d9
DESKTOP-FRK722U\Thilina <dilshanthilina53@gmail.com>
2021-05-06 18:47:05 +0530

    better

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   env: {
     cloud_name: "ddo9tyz6e",
     api_key: "569274784458179",
@@ -11,4 +25,5 @@ module.exports = {
   future: {
     webpack5: true,
   },
+
 };
